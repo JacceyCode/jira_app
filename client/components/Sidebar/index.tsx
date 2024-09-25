b84eebecadc9a7b52d34bb5d848026b6ece71146
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2024-09-25 16:02:19 +0100

    change in navbar style

--- client/components/Sidebar/index.tsx
+++ client/components/Sidebar/index.tsx
@@ -53,7 +53,7 @@ const Sidebar = () => {
 
   return (
     <aside className={`${sidebarClassNames}`}>
-      <section className="felx h-[100%] w-full flex-col justify-start">
+      <section className="flex h-[100%] w-full flex-col justify-start">
         {/* TOP LOGO */}
         <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
           <div className="text-xl font-bold text-gray-800 dark:text-white">
